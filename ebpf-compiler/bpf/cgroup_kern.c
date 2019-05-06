e96f77dc9d9e8590aeb0df3930c105905fce926f
elfadel <bonfohelfadel@gmail.com>
2019-05-06 05:57:06 +0200

    Update cgroup_kern.c

--- ebpf-compiler/bpf/cgroup_kern.c
+++ ebpf-compiler/bpf/cgroup_kern.c
@@ -21,26 +21,23 @@
 #include <linux/net.h>
 #include <uapi/linux/in.h>
 
-
 #include "bpf_helpers.h"
-
-
 
 
 SEC("cgroup/sock1")
 int bpf_prog_cgroup(struct bpf_sock *sk)
 {	
 
-	//__u32 proto = bpf_ntohs(sk->protocol);
+    //__u32 proto = bpf_ntohs(sk->protocol);
     char fmt[] = "socket: family %d type %d protocol %d\n";
 
     bpf_trace_printk(fmt, sizeof(fmt), sk->family, sk->type, sk->protocol);
-	if (sk->family == PF_INET &&
+    if (sk->family == PF_INET &&
         sk->type == SOCK_RAW &&
         sk->protocol == IPPROTO_ICMP)
 		return 0;
 
-	return 1;
+    return 1;
 }
 
 char _license[] SEC("license") = "GPL";
