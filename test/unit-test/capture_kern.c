135fdcd08fea26f7694c8ad16e11c8b4ff567a46
Ezechiel Tapé <cedric.tape.e@gmail.com>
2019-07-22 17:12:45 +0200

    injection and capture code source

---
+++ test/unit-test/capture_kern.c
@@ -0,0 +1,56 @@
+/*
+ * kernel code of launcher
+ */
+
+
+#define KBUILD_MODNAME "foo"
+
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+
+#include <linux/bpf.h>
+#include "../../bpf/bpf_helpers.h"
+#include "../../bpf/bpf_endian.h"
+
+#include "../../config.h"
+#include "../../lib/maps.h"
+
+#define IPPROTO_VTL 200
+struct vtlhdr{
+	int value;
+};
+
+SEC("xdp")
+int xdp_capture_vtl_program(struct xdp_md *ctx)
+{
+    void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
+
+    struct ethhdr *eth = data;
+
+    if (data + sizeof(*eth) > data_end)
+		return XDP_DROP; // Est-ce la bonne action ?
+
+    if((void *)eth + 1 > data_end)
+        return XDP_DROP;
+
+    if(eth->h_proto != ETH_P_IP)
+        return XDP_PASS;
+    
+    struct iphdr *iph = data + sizeof(*eth);
+    u8 iph_len = iph->ihl << 2;
+
+    if((void *)iph + 1 > data_end)
+        return XDP_PASS;
+
+    if(iph->protocol != IPPROTO_VTL)
+        return XDP_PASS;
+
+    //struct vtlhdr *vtlh = (struct vtlhdr *)(data + ETH_HLEN + iph_len);
+    //transmit data to userspace
+    
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
