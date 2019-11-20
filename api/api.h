4ecae6565d3a7c37ade78109de2564b229661c0a
Ezechiel Tapé <cedric.tape.e@gmail.com>
2019-11-20 09:25:22 +0100

    api: update api.h

--- api/api.h
+++ api/api.h
@@ -2,6 +2,11 @@
 #ifndef __API_H
 #define __API_H
 
+#include "../include/vtl/vtl_structures.h"
+
+int vtl_config(vtl_md_t *vtl_md, char *interface, char *src_ip);
+int vtl_snd(vtl_md_t *vtl_md, char *dst_ip, uint8_t *data,  size_t datalen);
+ssize_t vtl_rcv(vtl_md_t *vtl_md, void *buf);
 
 
 
