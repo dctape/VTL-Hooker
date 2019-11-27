c1ee5f2b2d88ae4641e123fc2eeb1f3611489bf2
Ezechiel Tapé <cedric.tape.e@gmail.com>
2019-11-27 11:42:32 +0100

    add unit tests repo

---
+++ tests/unit-tests/unit-tests.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <pcap.h>
+
+void select_interface(char *interface)
+{
+        pcap_if_t *alldevsp, *device;
+        char errbuf[100], devs[100][100];
+        int count = 1, n;
+
+        //First get the list of available devices
+        printf("Finding available devices ... ");
+        if (pcap_findalldevs(&alldevsp, errbuf))
+        {
+                printf("Error finding devices : %s", errbuf);
+                exit(1);
+        }
+        printf("Done");
+
+        //Print the available devices
+        printf("\nAvailable Devices are :\n");
+        for (device = alldevsp; device != NULL; device = device->next)
+        {
+                printf("%d. %s - %s\n", count, device->name, device->description);
+                if (device->name != NULL)
+                {
+                        strcpy(devs[count], device->name);
+                }
+                count++;
+        }
+        //Ask user which device to use
+        printf("Enter the number of the device you want use : ");
+        scanf("%d", &n);
+        
+        //interface = devs[n];
+        strcpy(interface, devs[n]);
+}
+
+int main()
+{
+        char interface[512];
+
+        select_interface(interface);
+        printf("\nselected interface : %s - size: %ld\n",interface
+                , strlen(interface));
+
+        return 0;
+}
